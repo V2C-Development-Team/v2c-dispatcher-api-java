4964bbde5b6f32c587f3ab171317343db242f59d
Caleb L. Power <cpower@axonibyte.com>
2020-11-21 00:03:29 -0600

    updated license to Apache 2.0 in accordance with the wishes of the Council.

--- src/main/java/edu/uco/cs/v2c/dispatcher/api/payload/incoming/InboundConfigUpdatePayload.java
+++ src/main/java/edu/uco/cs/v2c/dispatcher/api/payload/incoming/InboundConfigUpdatePayload.java
@@ -1,8 +1,10 @@
 /*
  * Copyright (c) 2020 V2C Development Team. All rights reserved.
- * Licensed under the Version 0.0.1 of the V2C License (the "License").
- * You may not use this file except in compliance with the License.
- * You may obtain a copy of the License at <https://tinyurl.com/v2c-license>.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
  * 
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
